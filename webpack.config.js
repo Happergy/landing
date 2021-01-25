e6b1e4051147d124b15772500dc7eda3c69bb592
Marc Ferret <marc.ferret@mango.com>
2021-01-25 08:15:46 +0100

    feat: add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const path = require('path');
+
+module.exports = () => {
+  // Entry path
+  const entryPath = path.join('styles');
+  const entryFile = 'styles.scss';
+  const outputPath = 'dist';
+
+  return {
+    entry: path.join(__dirname, entryPath, entryFile),
+    output: {
+      path: path.join(__dirname, outputPath),
+    },
+    module: {
+      rules: [
+        // SCSS
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                config: {
+                  path: './webpack/postcss.config.js',
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sassOptions: {
+                  includePaths: ['node_modules'],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: (() => ([
+      new FixStyleOnlyEntriesPlugin(),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: './public',
+          to: '',
+        },
+      ]),
+    ]))(),
+  };
+};
